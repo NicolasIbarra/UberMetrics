2f645bfb2cfca0176b6ecdec47c2739cc40ef7e7
NicolasIbarra <nicolasibarra84@gmail.com>
2017-09-07 20:59:20 -0300

    Botones funcionando del tutorial

--- button.php
+++ button.php
@@ -6,26 +6,47 @@ function get_buttons() {
 		1 => 'Save',
 		2 => 'Cancel',
 		3 => 'Refresh',
-		4 => 'Delete';
-		5 => 'Business Operation'
+		4 => 'Delete',
+		5 => 'Business Operation',
 	);
 
-	while(list($k, $v)=each($btns)) {
-		$str .= '<input type="submit" value="'.$v.'" name="btn_'$k'" id="btn_'$k'" />'; 
+	while(list($k,$v) = each($btns)) 
+	{
+		$str.= '<input type="submit" value="'.$v.'" name="btn_'.$k.'" id="btn_'.$k.'" />';
 	}
 	return $str;
 }
 
-
 ?>
 
-
+<!DOCTYPE html>
 <html>
 
 <body>
-	<div id="buttons_panel">
-		<?php echo get_buttons(); ?>
-	</div>
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		<div id="buttons_panel">
+			<?php echo get_buttons(); ?>
+			<?php 
+				echo "<br />";
+				if(isset($_POST['btn_1'])) {
+				print "Saved Clicked";
+				}
+				if(isset($_POST['btn_2'])) {
+				print "Cancel Clicked";
+				}
+				if(isset($_POST['btn_3'])) {
+				print "Refresh Clicked";
+				}
+				if(isset($_POST['btn_4'])) {
+				print "Delete Clicked";
+				}
+				if(isset($_POST['btn_5'])) {
+				print "B. Op Clicked";
+				}
+
+			?>
+		</div>
+	</form>
 </body>
 
 </html>
